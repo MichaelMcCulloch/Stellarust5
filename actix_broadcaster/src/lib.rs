9b87a38e7fdf4710986e3c77cbff61642f4e1f77
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-09-10 05:41:34 -0600

    using a rw lock avoids the scenario when one client misses a message because it's being pinged

--- actix_broadcaster/src/lib.rs
+++ actix_broadcaster/src/lib.rs
@@ -6,20 +6,21 @@ use rayon::{prelude::*, ThreadPoolBuilder};
 use serde::Serialize;
 use std::{
     pin::Pin,
+    sync::{Arc, RwLock},
     task::{Context, Poll},
     time::Duration,
 };
 use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
 pub struct Broadcaster {
-    producer: Producer<UnboundedSender<Bytes>>,
-    consumer: Consumer<UnboundedSender<Bytes>>,
+    clients: Arc<RwLock<Vec<UnboundedSender<Bytes>>>>,
 }
 impl Broadcaster {
     const PING_INTERVAL: u64 = 10;
 
     pub fn create() -> Self {
-        let (producer, consumer) = queue(16, 16);
-        let me = Broadcaster { producer, consumer };
+        let me = Broadcaster {
+            clients: Arc::new(RwLock::new(vec![])),
+        };
         me.spawn_ping();
         me
     }
@@ -31,7 +32,7 @@ impl Broadcaster {
             .send(Bytes::from("event: connected\ndata: connected\n\n"))
             .unwrap();
 
-        self.producer.produce(bytes_sender).unwrap();
+        self.clients.write().unwrap().push(bytes_sender);
 
         Client(bytes_receiver)
     }
@@ -47,7 +48,7 @@ impl Broadcaster {
             ))
             .unwrap();
         log::info!("sending {} to new client", message_string);
-        self.producer.produce(bytes_sender).unwrap();
+        self.clients.write().unwrap().push(bytes_sender);
 
         Client(bytes_receiver)
     }
@@ -57,51 +58,31 @@ impl Broadcaster {
         let message_bytes =
             Bytes::from(["event: message\ndata: ", message_string, "\n\n"].concat());
 
-        let p = self.producer.clone();
-        let c = self.consumer.clone();
-
-        let mut write_back = vec![];
-
-        while let Ok(sender) = c.consume() {
-            write_back.push(sender);
-        }
-        log::info!("sending {} to {} clients", message_string, write_back.len());
-
-        // high priority: do in parallel
-        write_back.par_drain(..).for_each(|sender| {
-            sender.send(message_bytes.clone()).unwrap();
-            p.produce(sender).unwrap();
-        });
+        self.clients
+            .read()
+            .unwrap()
+            .par_iter()
+            .for_each(|sender| sender.send(message_bytes.clone()).unwrap());
     }
 
     fn spawn_ping(&self) {
-        let producer = self.producer.clone();
-        let consumer = self.consumer.clone();
+        let clients = self.clients.clone();
         actix_web::rt::spawn(async move {
             let mut task = interval_at(Instant::now(), Duration::from_secs(Self::PING_INTERVAL));
             loop {
                 task.tick().await;
-                Self::remove_stale_clients(&producer, &consumer).await;
+                Self::remove_stale_clients(&clients).await;
             }
         });
     }
-    async fn remove_stale_clients(
-        producer: &Producer<UnboundedSender<Bytes>>,
-        consumer: &Consumer<UnboundedSender<Bytes>>,
-    ) {
-        let mut write_back = vec![];
-
-        // this is a low priority task: One at a time
-        while let Ok(sender) = consumer.consume() {
+    async fn remove_stale_clients(clients: &Arc<RwLock<Vec<UnboundedSender<Bytes>>>>) {
+        clients.write().unwrap().retain(|sender| {
             if let Ok(()) = sender.send(Bytes::from("event: ping\ndata: ping\n\n")) {
-                write_back.push(sender);
+                true
+            } else {
+                false
             }
-        }
-
-        log::info!("Retaining {} clients", write_back.len());
-        while let Some(sender) = write_back.pop() {
-            producer.produce(sender).unwrap();
-        }
+        });
     }
 }
 pub struct Client(UnboundedReceiver<Bytes>);
