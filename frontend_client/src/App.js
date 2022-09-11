69a732c2c1c54a9314d327975348f896a4e64233
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-09-10 20:31:21 -0600

    app

--- frontend_client/src/App.js
+++ frontend_client/src/App.js
@@ -1,23 +1,58 @@
+import React from 'react';
 import './App.css';
-import logo from './logo.svg';
+
+function CampaignButton(props) {
+  return <li>
+    <div>
+      {props.campaign_name}
+    </div>
+    <div>
+      {props.empire_list.map((a) => <div>{a.name} {a.player}</div>)}
+    </div>
+  </li>;
+}
+function CampaignSelect(props) {
+  return <div>
+    {Object.entries(props.data).map(([campaign_name, campaign_info_struct]) => <CampaignButton campaign_name={campaign_info_struct.campaign_name} empire_list={campaign_info_struct.empire_list} />)}
+  </div>;
+}
+
+class CampaignSelectPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+  }
+
+  componentDidMount() {
+    this.eventSource = new EventSource("//localhost:8000/campaigns");
+
+    this.eventSource.onmessage = (e) => {
+      this.setState(JSON.parse(e.data));
+    }
+  }
+  componentWillUnmount() {
+    this.eventSource.close()
+  }
+  render() {
+    if (this.state != {}) {
+
+      return (<div><CampaignSelect data={this.state} /></div>)
+    } else {
+      return (<div>404</div>)
+
+    }
+
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <CampaignSelectPage />
     </div>
   );
 }
