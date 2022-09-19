74f12aab3f9fde01acb6b89ff4702d672865803f
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-09-19 11:34:34 -0600

    Log info for model removals

--- game_data_controller/src/lib.rs
+++ game_data_controller/src/lib.rs
@@ -113,7 +113,7 @@ impl GameModelController {
                 actix_rt::spawn(async move {
                     match receiver.recv().await.unwrap() {
                         () => {
-                            log::trace!("Removing entry for {:?}", model_spec_enum);
+                            log::info!("Removing entry for {:?}", model_spec_enum);
                             broadcasters_map.remove(&model_spec_enum)
                         }
                     }
