9758caa5bce1dab135ad9fe662f620f2888520a0
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-09-22 06:24:11 -0600

    task is cross platform

--- .vscode/deploy_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd $1;
-systemfd --no-pid -s http::$2 -- \
-cargo-watch \
---ignore 'frontend_client/*' \
---ignore '.vscode/*' \
---ignore '.git/*' \
--x 'run --package actix_server --bin actix_server_bin --release';
