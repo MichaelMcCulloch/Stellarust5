fa4bf72f8391823784336b286733f621673047da
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-09-28 12:32:34 -0600

    complete game

--- production_data/prepare_data.sh
+++ production_data/prepare_data.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-tar -xf $1.tar.xz
-(
-    cd $1
-    for campaign in ./*
-    do
-        (
-            cd $campaign
-            for save in ./*
-            do
-                (
-                    cd $save
-                    zip -r ../$save.sav ./meta ./gamestate
-                    
-                )
-                rm -r $save
-                
-            done
-            
-        )
-    done
-)
+tar -xf production_data.tar.xz
+for version in "3.4.5.95132" "3.5.1.98532"
+do
+    (
+        cd ./$version;
+        for campaign in ./*/
+        do
+            (
+                cd ./$campaign
+                for save in ./*
+                do
+                    (
+                        cd ./$save
+                        zip -r ../$save.sav ./meta ./gamestate
+                    )
+                    rm -r $save
+                done
+            )
+        done
+    )
+    
+done
