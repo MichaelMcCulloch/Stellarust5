41965d5b072b6b5a11bbac501a805f9ed8a24234
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-09-28 07:21:08 -0600

    move to constants

--- game_data_controller/src/ctrlr/mod.rs
+++ game_data_controller/src/ctrlr/mod.rs
@@ -21,6 +21,12 @@ use std::hash::BuildHasherDefault;
 use std::path::Path;
 use std::sync::Arc;
 
+const SQL_SEL_TABLE_NAMES: &str =
+    "SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';";
+const SQL_CREATE_TABLE_DATA_POINTS: &str = "CREATE TABLE data_points ( blob TEXT NOT NULL );";
+const SQL_SEL_DATA_POINTS: &str = "SELECT * FROM data_points;";
+const SQL_INSERT_DATA_POINT: &str = "INSERT INTO data_points (blob) values (?)";
+
 pub(crate) fn get_directory_watcher(
     info_struct_sender: Sender<ModelDataPoint>,
     game_directory: &Path,
@@ -40,25 +46,19 @@ pub(crate) fn get_directory_watcher(
     )
 }
 pub(crate) fn query_models(db_connection: &Connection) -> anyhow::Result<Vec<ModelDataPoint>> {
-    let sql_select_table_names =
-        "SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';";
-    let sql_create_table_data_points = "CREATE TABLE data_points (
-            blob    TEXT NOT NULL
-        );";
-    let sql_select_all_data = "SELECT * FROM data_points;";
-
     let tables = db_connection
-        .prepare(sql_select_table_names)?
+        .prepare(SQL_SEL_TABLE_NAMES)?
         .query_map([], |row| row.get::<_, String>(0))?
         .filter_map(|s| s.ok())
         .collect::<Vec<_>>();
+
     if !tables.contains(&"data_points".to_string()) {
-        db_connection.execute(sql_create_table_data_points, [])?;
+        db_connection.execute(SQL_CREATE_TABLE_DATA_POINTS, [])?;
         log::info!("Populating empty database `stellarust_model_history.db` in game folder");
         Ok(vec![])
     } else {
         let extant_data = db_connection
-            .prepare(sql_select_all_data)?
+            .prepare(SQL_SEL_DATA_POINTS)?
             .query_map([], |row| row.get::<_, String>(0))?
             .filter_map(|s| s.ok())
             .filter_map(|s| serde_json::from_str(s.as_str()).ok())
@@ -104,9 +104,7 @@ pub(crate) fn spawn_event_loop(
 
 pub(crate) fn write_to_db(data_point: &ModelDataPoint, db_connection: &Connection) {
     let msg = serde_json::to_string(data_point).unwrap();
-    db_connection
-        .execute("INSERT INTO data_points (blob) values (?)", [msg])
-        .unwrap();
+    db_connection.execute(SQL_INSERT_DATA_POINT, [msg]).unwrap();
 }
 pub(crate) fn broadcast_model_changes(
     broadcasters_map: &Arc<
