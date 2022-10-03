0828aedca1ae80f828195a3b327dcd255de8243a
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-10-03 11:03:58 -0600

    avold rendering if it would cause an error

--- frontend_client/src/ResourceSummary.js
+++ frontend_client/src/ResourceSummary.js
@@ -28,33 +28,49 @@ class ResourceSummary extends React.Component {
         this.state = { data: [] }
 
     }
-    componentDidMount() {
-        console.log("componentDidMount");
-
-        this.createEventSource();
-
-
-    }
+    componentDidMount() { this.createEventSource(); }
     componentDidUpdate(prevProps) {
-        console.log("componentDidUpdate");
         if (this.props.resources != prevProps.resources) // Check if it's a new user, you can also use some unique property, like the ID  (this.props.user.id !== prevProps.user.id)
         {
-            this.eventSource.close()
+            if (this.eventSource) { this.eventSource.close() }
             this.state.data = [];
-
             this.createEventSource();
         }
+    }
 
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.props.resources !== nextProps.resources) // Check if it's a new user, you can also use some unique property, like the ID  (this.props.user.id !== prevProps.user.id)
+        {
+
+            if (this.eventSource) { this.eventSource.close() }
+            this.state.data = [];
+            this.eventSource = new EventSource(GET_REMOTE_HOST(this.props.campaign_name + "/" + this.props.empire_name + "/resourcesummary/" + nextProps.resources.join("")));
+
+            this.eventSource.onmessage = (e) => {
+                const new_data = JSON.parse(e.data).ResourceSummary.map(x => [x[0]].concat(x[1]));
+                this.setState({ data: this.state.data.concat(new_data) });
+
+            };
+            return false
+        } else if (this.state.data !== nextState.data) {
+            if (nextState.data[0].length === nextProps.resources.length + 1) {
+
+                return true
+
+            } else {
+
+                return false
+            }
+        } else {
+
+            return false
+        }
     }
 
     createEventSource() {
-        console.log("createEventSource");
-        let resource_code_string = this.props.resources.join("");
-
-        this.eventSource = new EventSource(GET_REMOTE_HOST(this.props.campaign_name + "/" + this.props.empire_name + "/resourcesummary/" + resource_code_string));
 
 
-
+        this.eventSource = new EventSource(GET_REMOTE_HOST(this.props.campaign_name + "/" + this.props.empire_name + "/resourcesummary/" + this.props.resources.join("")));
 
         this.eventSource.onmessage = (e) => {
             const new_data = JSON.parse(e.data).ResourceSummary.map(x => [x[0]].concat(x[1]));
@@ -63,17 +79,16 @@ class ResourceSummary extends React.Component {
         };
     }
     componentWillUnmount() {
-        console.log("componentWillUnmount");
+
 
         this.eventSource.close()
     }
     render() {
-        console.log("render");
+
 
         if (this.state.data.length > 1) {
             if (this.props.empire_name && this.props.campaign_name) {
                 let data = [["Date"].concat(this.props.resources)].concat(this.state.data)
-                console.log(data);
                 return <ResourceSummaryChart data={data} />
             } else {
                 return <></>
